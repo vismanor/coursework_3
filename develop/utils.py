754c3f74f73aaa1f31b47d07596a8e7bf65b14ad
Katya Smirnova <vismanork@gmail.com>
2024-01-11 21:36:49 +0300

    get_executed_operations and get_operations_with_from funcs were created

--- develop/utils.py
+++ develop/utils.py
@@ -12,3 +12,17 @@ def load_data(filename):
         """
         data = json.load(file)
         return data
+
+
+def get_executed_operations(data):
+    """
+            Фильтруем все операции, чтобы получить только со статусом 'executed'
+    """
+    return [operation for operation in data if "state" in operation and operation["state"] == "EXECUTED"]
+
+
+def get_operations_with_from(operations):
+    """
+                Фильтруем все операции, чтобы получить только с пунктом 'from'
+    """
+    return [operation for operation in operations if "from" in operation]
